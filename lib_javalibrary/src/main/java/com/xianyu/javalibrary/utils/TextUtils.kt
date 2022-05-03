39c3ba5680c875fcad0403d64c725bb1bdc48c37
chenxianyu52 <380286681@qq.com>
2022-05-03 16:43:10 +0800

    lib_javalibrary git init

---
+++ lib_javalibrary/src/main/java/com/xianyu/javalibrary/utils/TextUtils.kt
@@ -0,0 +1,13 @@
+package com.xianyu.javalibrary.utils
+
+object TextUtils {
+    /**
+     * Returns true if the string is null or 0-length.
+     * @param str the string to be examined
+     * @return true if str is null or zero length
+     */
+    fun isEmpty(str: CharSequence?): Boolean {
+        return str == null || str.isEmpty()
+    }
+
+}
